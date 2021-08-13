cd3e4ccff88d19cd8bd48fb2186f7127d9f0b35c
hon <hon102561@gmail.com>
2021-08-13 19:06:43 +0900

    課題1を提出

--- 課題1-2.py
+++ 課題1-2.py
@@ -20,7 +20,7 @@ while True:
 
             is_add = input("追加登録しますか？(n:しない　y:する)")
 
-            # ここですが、この位置に書くと、else存在しませんの場合に、追加登録is_addが表示するようにできるのでしょうか？ 
+            # ここですが、この位置に書くと、else存在しませんの場合に、追加登録is_addが表示できるようになる
             if is_add=="y":
                 with open(SOURCE_CSV_PATH, mode="a", encoding="utf-8_sig") as f:
                     f.write("\n" + word)
